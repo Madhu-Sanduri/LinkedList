b24986e084494a6424cff21cc5b94696cd181c01
Madhu-Sanduri <madhusanduri343@gmail.com>
2025-05-07 22:34:47 +0530

    LinkedList

--- src/Palindrome_LL.java
+++ src/Palindrome_LL.java
@@ -44,6 +44,47 @@ public class Palindrome_LL {
         return head;
     }
 
+    public Node rev(Node head){
+        Node temp=head;
+        Node next=null;
+        Node prev=null;
+
+        while (temp!=null){
+            next=temp.next;
+            temp.next=prev;
+            prev=temp;
+            temp=next;
+        }
+
+        return prev;
+    }
+
+    public boolean palindrome(Node head){
+        Node slow=head;
+        Node fast=head;
+
+        while (fast.next!=null && fast.next.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+        Node newHead=rev(slow.next);
+        Node firstHead=head;
+        Node secondHead=newHead;
+        display(firstHead);
+        display(secondHead);
+
+        while (secondHead!=null){
+            if(firstHead.value!=secondHead.value) {
+                rev(newHead);
+                return false;
+            }
+            secondHead=secondHead.next;
+            firstHead=firstHead.next;
+        }
+        rev(newHead);
+        return true;
+    }
+
     public boolean palindrome_stack(Node head){
         Stack<Integer> stack=new Stack<>();
         Node temp=head;
@@ -62,7 +103,7 @@ public class Palindrome_LL {
         return true;
     }
 
-    public void display(){
+    public void display(Node head){
         Node temp=head;
 
 
@@ -77,17 +118,26 @@ public class Palindrome_LL {
         Palindrome_LL demo = new Palindrome_LL();
         demo.insertLast(1);
         demo.insertLast(2);
-        demo.insertLast(1);
-        demo.insertLast(2);
         demo.insertLast(3);
         demo.insertLast(4);
+        demo.insertLast(5);
+        demo.insertLast(6);
+        demo.insertLast(3);
+        demo.insertLast(2);
+        demo.insertLast(1);
 //        demo.insertLast(5);
-        demo.insertLast(4);
-        demo.insertLast(4);
-        demo.insertLast(3);
-        demo.insertLast(2);
-        demo.insertLast(1);
-        demo.display();
-        System.out.println( demo.palindrome_stack(demo.head));
+//        demo.insertLast(6);
+//        demo.insertLast(1);
+//        demo.insertLast(2);
+//        demo.insertLast(4);
+////        demo.insertLast(5);
+//        demo.insertLast(4);
+//        demo.insertLast(4);
+//        demo.insertLast(3);
+//        demo.insertLast(2);
+//        demo.insertLast(1);
+//        demo.display();
+//        System.out.println( demo.palindrome_stack(demo.head));
+        System.out.println( demo.palindrome(demo.head));
     }
 }
---
+++ src/odd_even_linkedlist.java
@@ -0,0 +1,86 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class odd_even_linkedlist {
+
+    public Node head;
+    public Node tail;
+    public int size;
+
+    public odd_even_linkedlist(){
+        size=0;
+    }
+
+    public class Node{
+        private int value;
+        private Node next;
+
+        public Node(int value) {
+            this.value = value;
+        }
+
+        public Node(int value, Node next) {
+            this.value = value;
+            this.next = next;
+        }
+    }
+
+    public Node solution(Node head){
+        List<Integer> list = new ArrayList<>();
+
+        Node temp=head;
+
+        while (temp.next!=null || temp!=null){
+            list.add(temp.value);
+            temp=temp.next;
+        }
+        System.out.println(list);
+        return head;
+    }
+
+/*----------------------------------------------------------------------------------------------------------------------------------------------*/
+    public Node insertLast(int value){
+        Node node = new Node(value);
+        size++;
+        if(head==null){
+            node.next=head;
+            head=node;
+            return head;
+        }
+        Node temp=head;
+        while (temp.next!=null){
+            temp=temp.next;
+        }
+
+        temp.next=node;
+        node.next=null;
+        return head;
+
+
+    }
+
+    public void display(){
+        Node temp=head;
+
+        while (temp!=null){
+            System.out.print(temp.value+"-->");
+            temp=temp.next;
+        }
+        System.out.println("END");
+    }
+
+/*----------------------------------------------------------------------------------------------------------------------------------------------*/
+
+
+    public static void main(String[] args) {
+        odd_even_linkedlist demo = new odd_even_linkedlist();
+        int []a={1,2,3,4,5};
+        for(int i:a){
+            demo.insertLast(i);
+        }
+        demo.display();
+        System.out.println(demo.size);
+
+        demo.solution(demo.head);
+    }
+}
